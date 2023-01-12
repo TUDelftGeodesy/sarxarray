7ec339bdd8693b25efff80b98f768e56a6298227
rogerkuou <o.ku@esciencecenter.nl>
2023-01-12 14:34:06 +0100

    fix the runtime warnings

--- sarxarray/conf.py
+++ sarxarray/conf.py
@@ -1,4 +1,4 @@
 import numpy as np
 
 # Standard data types in sarxarray:
-_dtypes = dict(int=np.int32, float=np.float16, complex=np.complex64)
+_dtypes = dict(int=np.int32, float=np.float32, complex=np.complex64)
