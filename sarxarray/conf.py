494fa263826ee4bbdd448b62dd56f0823e2231a7
Ou Ku <o.ku@esciencecenter.nl>
2023-10-19 15:52:16 +0200

    update auto chunksize computation

--- sarxarray/conf.py
+++ sarxarray/conf.py
@@ -2,3 +2,6 @@ import numpy as np
 
 # Standard data types in sarxarray:
 _dtypes = dict(int=np.int32, float=np.float32, complex=np.complex64)
+
+# Optimal memory size of a chunk, in MB
+_memsize_chunk_mb = 100
