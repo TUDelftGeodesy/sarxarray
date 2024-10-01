b37f086948f821d7dd7781ef7da3f90650689f0f
Ou Ku <o.ku@esciencecenter.nl>
2024-10-01 16:17:06 +0200

    formatting

--- sarxarray/__init__.py
+++ sarxarray/__init__.py
@@ -2,4 +2,4 @@ from sarxarray import stack
 from sarxarray._io import from_binary, from_zarr
 from sarxarray.utils import complex_coherence, multi_look
 
-__all__ = ("stack", "from_binary", "multi_look", "complex_coherence")
+__all__ = ("stack", "from_binary", "from_zarr", "multi_look", "complex_coherence")
